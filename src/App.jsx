446b7cb82b8876272ea50207da8e83dac17a8bc1
Thiago_19 Notebook <riffothiago19@gmail.com>
2026-06-13 17:00:13 -0300

    Mejorando el router agregando algunas de las vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Route, Switch, Link } from "wouter";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Home from "./pages/Home/Home"
+import TerminosYCondiciones from "./pages/TerminosYCondiciones/TerminosYCondiciones";
+import SobreNosotros from "./pages/SobreNosotros/SobreNosotros";
+import PreguntasFrecuentes from "./pages/PreguntasFrecuentes/PreguntasFrecuentes";
 
 import './App.css'
+
 export default function App() {
   return (
     <div>
@@ -13,15 +19,15 @@ export default function App() {
       </nav>
 
       <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/about">
-          <div>Contenido de Acerca de</div>
-        </Route>
+        <Route path="/home" component={Home} />
+        {/* <Route path="/catalogo" component={Catalogo} /> */}
+        <Route path="/sobrenosotros" component={SobreNosotros} />
+        <Route path="/faqs" component={PreguntasFrecuentes} />
+        <Route path="/terminos" component={TerminosYCondiciones} />
+        <Route path="/" ><div>Contenido de Acerca de</div></Route>
         <Route>404: ¡Página no encontrada!</Route>
       </Switch>
       <Footer />
-    </div>
+    </div >
   );
 }
-
-const Home = () => <h2>Inicio</h2>;
