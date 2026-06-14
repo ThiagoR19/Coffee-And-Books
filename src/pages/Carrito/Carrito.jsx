9667de0373fc094159dfa61f6ccc3462cb280c83
Thiago_19 Notebook <riffothiago19@gmail.com>
2026-06-14 16:07:29 -0300

    Arreglando error de importacion

--- src/pages/Carrito/Carrito.jsx
+++ src/pages/Carrito/Carrito.jsx
@@ -2,7 +2,7 @@ import ProductosCarrito from '../../components/productosCarrito/ProductosCarrito
 import ProductoCarrito from '../../components/ProductoCarrito/ProductoCarrito'
 import ResumenCarrito from '../../components/ResumenCarrito/ResumenCarrito'
 
-import '/Carrito.css'
+import './Carrito.css'
 
 const productos = [
   { id: 1, nombre: 'Nombre Producto', precio: 100000 },
