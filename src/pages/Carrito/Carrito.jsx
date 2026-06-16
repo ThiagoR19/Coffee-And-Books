f99bfc69d8d7f6ca70aa995b8261f33e3645344c
Aaron <aaronsanche60@gmail.com>
2026-06-16 00:33:05 -0300

    catalogo finalizado

--- src/pages/Carrito/Carrito.jsx
+++ src/pages/Carrito/Carrito.jsx
@@ -28,7 +28,7 @@ function Carrito() {
               key={producto.id}
               className="producto"
             >
-              <img className='producto__img' src={cafeDefault} alt="Café por defecto" />
+              <img className='img-car' className='producto__img' src={cafeDefault} alt="Café por defecto" />
               <div className='producto__div'>
                 <h4 className='producto__div-h4'>{producto.nombre}</h4>
                 <span className='producto__div-span'>{producto.precio}</span>
@@ -38,7 +38,7 @@ function Carrito() {
           })}
         </div>
         <div className='productos__div'>
-          <img className='productos__div-img' src={iconoWhatsapp} alt="Icono de Whatsapp" />
+          <img className='img-car'className='productos__div-img' src={iconoWhatsapp} alt="Icono de Whatsapp" />
           <div className='productos__div-div'>
             <h3 className='productos__div-div-h3'>¿No encontraste tu producto?</h3>
             <span className='productos__div-div-span'>No dudes en pedirme que lo agregue al catálogo</span>
@@ -54,10 +54,10 @@ function Carrito() {
           <div className='producto__div-div'>
             <h2 className='producto__div-div-h2'>Café Colombiano Importado</h2>
           </div>
-          <img className='producto__div-img' src={cafeDefault} alt="" />
+          <img className='img-car'className='producto__div-img' src={cafeDefault} alt="" />
           <div className='producto__div-div'>
             <button className='producto__div-div-button'>
-              <img className='producto__div-div-button-img' src={buttonMinus} alt="" />
+              <img className='img-car'className='producto__div-div-button-img' src={buttonMinus} alt="" />
             </button>
             <div className='producto__div-div-div'>
               <h3 className='producto__div-div-div-h3'>3</h3>
@@ -97,9 +97,9 @@ function Carrito() {
           <h5 className='resumen__div-h5'>Total</h5>
           <span className='resumen__div-span'>$65.000</span>
         </div>
-        <button className='resumen__button'>Finalizar Compra <img src={iconoBolsa} alt="" /></button>
+        <button className='resumen__button'>Finalizar Compra <img className='img-car'src={iconoBolsa} alt="" /></button>
         <div className='resumen__div'>
-          <img className='resumen__div-img' src={iconoCheck} alt="" />
+          <img className='img-car'className='resumen__div-img' src={iconoCheck} alt="" />
           <div className='resumen__div-div'>
             <h3 className='resumen__div-div-h3'>Compra 100% segura</h3>
             <span className='resumen__div-div-span'>Tus datos están protegidos en todo momento</span>
