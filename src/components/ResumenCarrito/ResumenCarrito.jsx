65e662bdee97f1968e690d590dbbf6566fb7eca3
ThiagoR19 <riffothiago19@gmail.com>
2026-06-16 00:42:04 -0300

    Avanzando con la vista de carrito

--- src/components/ResumenCarrito/ResumenCarrito.jsx
+++ src/components/ResumenCarrito/ResumenCarrito.jsx
@@ -1,10 +1,14 @@
 import iconoCheck from '../../assets/icon-check.png'
 import iconoBolsa from '../../assets/icon-bolsa.png'
 
+import './ResumenCarrito.css'
+
 function ResumenCarrito() {
   return (
     <article id="resumen">
-      <h3 className='resumen__h3'>Resumen de Compra</h3>
+      <div className='resumen__div'>
+        <h3 className='resumen__h3'>Resumen de Compra</h3>
+      </div>
       <div className='resumen__div'>
         <div className='resumen__div-div'>
           <h5 className='resumen__div-div-h5'>Café Colombiano...</h5>
@@ -15,19 +19,19 @@ function ResumenCarrito() {
           <span className='resumen__div-div-span'>$75.000</span>
         </div>
         <div className='resumen__div-div'>
-          <h5 className='resumen__div-div-h5'>Descuentos</h5>
-          <span className='resumen__div-div-span'>$-10.000</span>
+          <h5 className='resumen__div-div-h5 descuento'>Descuentos</h5>
+          <span className='resumen__div-div-span descuento-precio'>$-10.000</span>
         </div>
-        <div className='resumen__underline'></div>
       </div>
-      <div className='resumen__div'>
-        <h5 className='resumen__div-h5'>Total</h5>
-        <span className='resumen__div-span'>$65.000</span>
+      <div className='resumen__underline'></div>
+      <div className='resumen__div-total'>
+        <h5 className='resumen__div-h5 total'>Total</h5>
+        <span className='resumen__div-span total-precio'>$65.000</span>
       </div>
       <button className='resumen__button'>Finalizar Compra <img src={iconoBolsa} alt="" /></button>
-      <div className='resumen__div'>
+      <div className='resumen__div-segura'>
         <img className='resumen__div-img' src={iconoCheck} alt="" />
-        <div className='resumen__div-div'>
+        <div className='resumen__div-div-segura'>
           <h3 className='resumen__div-div-h3'>Compra 100% segura</h3>
           <span className='resumen__div-div-span'>Tus datos están protegidos en todo momento</span>
         </div>
