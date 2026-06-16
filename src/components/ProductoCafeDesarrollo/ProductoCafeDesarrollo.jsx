79de1332ac9e3fc86b2e92644105be4758f4b511
ThiagoR19 <riffothiago19@gmail.com>
2026-06-16 16:16:34 -0300

    Avanzando con la vista producto desarrollado cafe

--- src/components/ProductoCafeDesarrollo/ProductoCafeDesarrollo.jsx
+++ src/components/ProductoCafeDesarrollo/ProductoCafeDesarrollo.jsx
@@ -16,24 +16,24 @@ function ProductoCafeDesarrollo() {
   return (
     <article id='cafe'>
       <div id='imagenes'>
-        <img src={coffeeExample1} alt="Café de ejemplo" />
-        <div>
-          <img src={coffeeExample1} alt="Café de ejemplo" />
-          <img src={coffeeExample2} alt="Café de ejemplo" />
-          <img src={coffeeExample3} alt="Café de ejemplo" />
+        <img className='imagenes__img' src={coffeeExample1} alt="Café de ejemplo" />
+        <div className='imagenes__div'>
+          <img className='imagenes__div-img' src={coffeeExample1} alt="Café de ejemplo" />
+          <img className='imagenes__div-img' src={coffeeExample2} alt="Café de ejemplo" />
+          <img className='imagenes__div-img' src={coffeeExample3} alt="Café de ejemplo" />
         </div>
       </div>
       <div id='informacion'>
         <div id='titulos'>
-          <h2>Caramel Expresso</h2>
-          <div>
-            <h3>Sturbucks</h3>
-            <h2>$19.000</h2>
+          <h2 className='titulos__h2'>Caramel Expresso</h2>
+          <div className='titulos__div'>
+            <h3 className='titulos__div-h3'>Sturbucks</h3>
+            <h2 className='titulos__div-h2'>$19.000</h2>
           </div>
         </div>
         <div id='descripcion'>
-          <h4>Descripción</h4>
-          <p>Disfrutá de la experiencia Starbucks en la comodidad de tu casa. El Caramel Espresso combina la intensidad de granos de café 100% Arábica cuidadosamente seleccionados de América Latina con un perfil sutilmente dulce y un delicioso aroma a caramelo tostado.</p>
+          <h4 className='descripcion__h4'>DESCRIPCIÓN</h4>
+          <p className='descripcion__p'>Disfrutá de la experiencia Starbucks en la comodidad de tu casa. El Caramel Espresso combina la intensidad de granos de café 100% Arábica cuidadosamente seleccionados de América Latina con un perfil sutilmente dulce y un delicioso aroma a caramelo tostado.</p>
         </div>
         <div className='underline'></div>
         <ul id='lista'>
@@ -43,19 +43,19 @@ function ProductoCafeDesarrollo() {
         </ul>
         <div className='underline'></div>
         <div id='detalles'>
-          <h3>Detalles del café</h3>
-          <div><img src={iconMarca} alt="" /><span>Marca:</span><p>Sturbubcks</p></div>
-          <div><img src={iconTipo} alt="Icono Tipo" /><span>Tipo:</span><p>Cápsula</p></div>
-          <div><img src={iconIntensidad} alt="Icono Intensidad" /><span>Intensidad:</span><p>Sturbubcks</p></div>
-          <div><img src={iconOrigen} alt="Icono Origen" /><span>Origen:</span><p>America Latina</p></div>
-          <div><img src={iconContenido} alt="Icono Contenido" /><span>Contenido:</span><p>Caja de 10 capsulas</p></div>
+          <h3 className='detalles__h3'>DETALLES DEL CAFÉ</h3>
+          <div className='detalles__div'><img className='detalles__div-img' src={iconMarca} alt="" /><span>Marca:</span><p>Sturbubcks</p></div>
+          <div className='detalles__div'><img className='detalles__div-img' src={iconTipo} alt="Icono Tipo" /><span>Tipo:</span><p>Cápsula</p></div>
+          <div className='detalles__div'><img className='detalles__div-img' src={iconIntensidad} alt="Icono Intensidad" /><span>Intensidad:</span><p>Sturbubcks</p></div>
+          <div className='detalles__div'><img className='detalles__div-img' src={iconOrigen} alt="Icono Origen" /><span>Origen:</span><p>America Latina</p></div>
+          <div className='detalles__div'><img className='detalles__div-img' src={iconContenido} alt="Icono Contenido" /><span>Contenido:</span><p>Caja de 10 capsulas</p></div>
         </div>
         <div id='botones'>
-          <button><img src={iconoCarrito} alt="Icono del Carrito" />Agregar al carrito</button>
-          <div>
-            <button><img src={iconMinus} alt="Icono de resta" /></button>
-            <span>1</span>
-            <button><img src={iconPlus} alt="Icono de suma" /></button>
+          <button className='botones__button'><img src={iconoCarrito} alt="Icono del Carrito" />AGREGAR AL CARRITO</button>
+          <div className='botones__div'>
+            <button className='botones__div-button'><img src={iconMinus} alt="Icono de resta" /></button>
+            <span className='botones__div-span'>1</span>
+            <button className='botones__div-button'><img src={iconPlus} alt="Icono de suma" /></button>
           </div>
         </div>
       </div>
