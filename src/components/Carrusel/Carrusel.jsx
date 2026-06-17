6495d327647060914a8a772cf3e894fbb38c4eb1
ThiagoR19 <riffothiago19@gmail.com>
2026-06-17 15:16:49 -0300

    Avanzando con la vista prodCafeDesarrollado

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -9,17 +9,17 @@ import './Carrusel.css'
 function Carrusel() {
   return (
     <article id='carusel'>
-      <h3>Misma Marca</h3>
-      <div>
-        <img src={leftArrow} alt="Flecha Derecha" />
-        <div>
-          <img src={coffeeExample1} alt="Café de ejemplo" />
-          <img src={coffeeExample2} alt="Café de ejemplo" />
-          <img src={coffeeExample3} alt="Café de ejemplo" />
-          <img src={coffeeExample2} alt="Café de ejemplo" />
-          <img src={coffeeExample2} alt="Café de ejemplo" />
+      <h3 className='carusel__h3'>Misma Marca</h3>
+      <div className='carusel__div'>
+        <img className='carusel__div-img' src={leftArrow} alt="Flecha Derecha" />
+        <div className='carusel__div-div'>
+          <img className='carusel__div-div-img' src={coffeeExample1} alt="Café de ejemplo" />
+          <img className='carusel__div-div-img' src={coffeeExample2} alt="Café de ejemplo" />
+          <img className='carusel__div-div-img' src={coffeeExample3} alt="Café de ejemplo" />
+          <img className='carusel__div-div-img' src={coffeeExample2} alt="Café de ejemplo" />
+          <img className='carusel__div-div-img' src={coffeeExample2} alt="Café de ejemplo" />
         </div>
-        <img src={rightArrow} alt="Flecha Derecha" />
+        <img className='carusel__div-img' src={rightArrow} alt="Flecha Derecha" />
       </div>
     </article>
   )
