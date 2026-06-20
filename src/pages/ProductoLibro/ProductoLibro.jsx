184b9979ef5f57398daa0735be12b09b6a95f85c
Aaron <aaronsanche60@gmail.com>
2026-06-20 04:59:42 -0300

    me falta el carrousel

--- src/pages/ProductoLibro/ProductoLibro.jsx
+++ src/pages/ProductoLibro/ProductoLibro.jsx
@@ -4,11 +4,11 @@ import imagenGrande from "../../assets/productoLibro/grande.png"
 import chiquita1 from "../../assets/productoLibro/chiquita1.png"
 import chiquita2 from "../../assets/productoLibro/chiquita2.png"
 import chiquita3 from "../../assets/productoLibro/chiquita3.png"
-import iconUsuario from "../../assets/productoLibro/iconsPL/usuario-icon 3.svg"
-import iconLibro from "../../assets/productoLibro/iconsPL/icono-libro 5.svg"
-import iconIsbn from "../../assets/productoLibro/iconsPL/icon-isbn 2.svg"
-import iconEditorial from "../../assets/productoLibro/iconsPL/icon-editorial 2.svg"
-import iconDE from "../../assets/productoLibro/iconsPL/descuento-resumen 4.svg"
+import { FaRegUser } from "react-icons/fa6";
+import IconLibro from "../../assets/productoLibro/iconsPL/icono-libro 5.png"
+import IconIsbn from "../../assets/productoLibro/iconsPL/icon-isbn 2.png"
+import IconEditorial from "../../assets/productoLibro/iconsPL/icon-editorial 2.png"
+import IconDE from "../../assets/productoLibro/iconsPL/descuento-resumen 4.png"
 
 export default function ProductoLibro() {
     return (
@@ -22,22 +22,25 @@ export default function ProductoLibro() {
 
                 <div className="detalles-libro-1">
                     <div className="contenido-de-detalles">
-                        
-                        <h1 className="titulo-libro">Nombre del libro</h1>
+
+                        <h1 className="titulo-libro">Nombre del Libro</h1>
                         <p className="autor-libro">Autor del libro</p>
-                        <p className="precio-libro">$19.000</p>
 
-                        <div className="cont-de-atributos">
-                            <p className="genero">Género</p>
-                            <p className="formato">Formato</p>
+                        <div className="cont-auxiliar">
+                            <div className="lado-izquierdo">
+                                <p className="precio-libro">$19.000</p>
+                                <div className="cont-de-atributos">
+                                    <p className="genero">Género</p>
+                                    <p className="formato">Formato</p>
+                                </div>
+                            </div>
+                            <div className="bnt-copra-LP">
+                                <button className="boton-comprar">
+                                    <FaCartShopping className="icono-carrito" />AGREGAR AL CARRITO
+                                </button>
+                                <p className="UEC">Unidades en carrito: 1</p>
+                            </div>
                         </div>
-
-
-                        <div className="bnt-copra-LP">
-                            <button className="boton-comprar"><FaCartShopping className="icono-carrito" />Comprar</button>
-                            <p className="UEC">Unidadee en carrito: 1</p>
-                        </div>
-
 
                         <span className="titleSinopsisLP">SINOPSIS</span>
                         <p className="SinopsisLP">
@@ -49,6 +52,44 @@ export default function ProductoLibro() {
 
             <div className="detalles-libro-2">
 
+                <ul className="mi-listilla-pe">
+                    <h3 style={{ color: "white", fontFamily: "molengo" }}>DETALLES DEL LIBRO</h3>
+                    <li>
+                        <span className="icono-circulo">
+                            <FaRegUser style={{ color: "black" }} className="icono-lista" />
+                        </span>
+                        Autor:  Someone Somebody
+                    </li>
+                    <li>
+                        <span className="icono-circulo">
+                            <img className="icono-lista" src={IconDE} alt="Icono de libro" />
+                        </span>
+                        Género: Some gender
+                    </li>
+                    <li>
+                        <span className="icono-circulo">
+                            <img className="icono-lista" src={IconLibro} alt="Icono de usuario" />
+                        </span>
+                        Formato: a kind of format
+                    </li>
+                    <li>
+                        <span className="icono-circulo">
+                            <img className="icono-lista" src={IconEditorial} alt="Icono de usuario" />
+                        </span>
+                        Editorial: any
+                    </li>
+                    <li>
+                        <span className="icono-circulo">
+                            <img className="icono-lista" src={IconIsbn} alt="Icono de usuario" />
+                        </span>
+                        ISBN: 123-456-789-098-6
+                    </li>
+                </ul>
+                <div className="contenedor-de-imagenes">
+                    <img className="imagenesChiquitas" src={chiquita1} alt="Imagen de un libro" />
+                    <img className="imagenesChiquitas" src={chiquita2} alt="Imagen de un libro" />
+                    <img className="imagenesChiquitas" src={chiquita3} alt="Imagen de un libro" />
+                </div>
             </div>
 
             <div className="contenedor-de-autores">
